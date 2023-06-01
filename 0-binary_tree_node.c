17ef3254d3ebb24db4512ba960bb473ac1b36857
samierage <samierage@gmail.com>
2023-06-01 14:19:48 -0700

    C - Binary trees updated

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,8 @@
  *     *
  *      * Return: NULL If an error occurs.
  *       *         Otherwise - a pointer to the new node.
- *        */
+ *        *
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 		binary_tree_t *new;
